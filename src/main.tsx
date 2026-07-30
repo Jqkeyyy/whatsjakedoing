852cb783bad6839be575bc79bb193cc871d30fae
Jqkeyyy <146137316+Jqkeyyy@users.noreply.github.com>
2026-07-30 16:17:04 -0500

    feat: route /admin to AdminApp, everything else to the public App

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { AdminApp } from './admin/AdminApp';
 import './index.css';
 
+const isAdmin = window.location.pathname.startsWith('/admin');
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <StrictMode>{isAdmin ? <AdminApp /> : <App />}</StrictMode>
 );
